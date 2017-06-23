1eca5fad23d92dda19406c3fda7927a6d9f00740
NexeyaSGara <sebastien.gara@nexeya.com>
2017-06-23 11:52:25 +0200

    Update setenv_vc10.bat

--- appveyor/setenv_vc10.bat
+++ appveyor/setenv_vc10.bat
@@ -8,37 +8,37 @@
 :set_x86
 @echo Setting environment for using Microsoft Visual Studio 2010 x86 tools.
 
-set INCLUDE=^
-c:\Program Files\Microsoft Visual Studio 10.0\VC\include;^
-c:\Program Files\Microsoft SDKs\Windows\v7.0A\include;
+set INCLUDE=
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include;"
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\include;"
 
-set LIB=^
-c:\Program Files\Microsoft Visual Studio 10.0\VC\lib;^
-c:\Program Files\Microsoft SDKs\Windows\v7.0A\lib;
+set LIB=
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\lib;"
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\lib;"
 
-set PATH=^
-%SystemRoot%\system32;^
-c:\Program Files\Microsoft Visual Studio 10.0\VC\bin;^
-c:\program files\microsoft visual studio 10.0\common7\ide;
+set PATH=
+%SystemRoot%\system32;
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin;"
+"C:\Program Files (x86)\microsoft visual studio 10.0\common7\ide;"
 
 goto test_bin_locations
 
 :set_x64
 @echo Setting environment for using Microsoft Visual Studio 2010 x64 tools.
 
-set INCLUDE=^
-c:\Program Files\Microsoft Visual Studio 10.0\VC\include;^
-c:\Program Files\Microsoft SDKs\Windows\v7.0A\include;
+set INCLUDE=
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include;"
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\include;"
 
-set LIB=^
-c:\Program Files\Microsoft Visual Studio 10.0\VC\lib\amd64;^
-c:\Program Files\Microsoft SDKs\Windows\v7.0A\lib\x64;
+set LIB=
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\lib\amd64;"
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\lib\x64;"
 
-set PATH=^
-%SystemRoot%\system32;^
-C:\Program Files\Microsoft Visual Studio 10.0\VC\bin\x86_amd64;^
-C:\Program Files\Microsoft Visual Studio 10.0\VC\bin;^
-C:\Program Files\Microsoft Visual Studio 10.0\Common7\ide;
+set PATH=
+%SystemRoot%\system32;
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\x86_amd64;"
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin;"
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\ide;"
 
 goto test_bin_locations
 
