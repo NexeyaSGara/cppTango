bdaed470282e049304405380cdccdb28d5fa11cb
NexeyaSGara <sebastien.gara@nexeya.com>
2017-08-01 21:08:37 +0200

    Update cxx_signal.cpp

--- test/cpp_test_suite/new_tests/cxx_signal.cpp
+++ test/cpp_test_suite/new_tests/cxx_signal.cpp
@@ -255,7 +255,7 @@ public:
 		pid = atoi((*result).svalue[0].in());
 		if(pid > 0)
 		#ifdef _WIN32
-		 UINT uExitCode=0;
+		 __int8 uExitCode=0;
 		 DWORD dwDesiredAccess = PROCESS_TERMINATE;
 		 BOOL  bInheritHandle  = FALSE;
 		 HANDLE hProcess = OpenProcess(dwDesiredAccess, bInheritHandle, pid);
