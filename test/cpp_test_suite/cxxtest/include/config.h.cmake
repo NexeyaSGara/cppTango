87ac553b4c3a40bac85311e5e01a045a772e4e8a
NexeyaSGara <sebastien.gara@nexeya.com>
2017-08-01 15:18:36 +0200

    Update config.h.cmake

--- test/cpp_test_suite/cxxtest/include/config.h.cmake
+++ test/cpp_test_suite/cxxtest/include/config.h.cmake
@@ -4,9 +4,15 @@
 #include <string>
 
 namespace Tango {
+#ifdef _WIN32
+    const std::string kProjectBinaryDir = "@PROJECT_BINARY_DIR@";
+    const std::string kStartServerCmd = "@CMAKE_CURRENT_BINARY_DIR@/bin/start_server.sh ";
+    const std::string kKillServerCmd = "@CMAKE_CURRENT_BINARY_DIR@/bin/kill_server.sh";
+#else
     const std::string kProjectBinaryDir{"@PROJECT_BINARY_DIR@"};
     const std::string kStartServerCmd{"@CMAKE_CURRENT_BINARY_DIR@/bin/start_server.sh "};
     const std::string kKillServerCmd{"@CMAKE_CURRENT_BINARY_DIR@/bin/kill_server.sh"};
+#endif
 }
 
 #endif
