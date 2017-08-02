89e66b10af502c94b77524631dc2968e58395984
NexeyaSGara <sebastien.gara@nexeya.com>
2017-08-02 15:41:39 +0200

    Update cxx_class_dev_signal.cpp

--- test/cpp_test_suite/new_tests/cxx_class_dev_signal.cpp
+++ test/cpp_test_suite/new_tests/cxx_class_dev_signal.cpp
@@ -245,9 +245,7 @@ public:
 		pid = atoi((*result).svalue[0].in());
 		if(pid > 0)
 		#ifdef _WIN32
-		 int dwDesiredAccess = 0x0001;
-		 bool  bInheritHandle  = False;
-		 HANDLE hProcess = OpenProcess(dwDesiredAccess, bInheritHandle, pid);
+		 HANDLE hProcess = OpenProcess(0x0001, FALSE, pid);
 		 unsigned __int8 uExitCode = 0;
 		 TerminateProcess(hProcess, uExitCode);
 		 CloseHandle(hProcess);
