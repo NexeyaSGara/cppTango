adc969845ea7d1ee4b30654a7f4001e4d6699c0b
NexeyaSGara <sebastien.gara@nexeya.com>
2017-08-02 16:27:52 +0200

    Update cxx_class_signal.cpp

--- test/cpp_test_suite/new_tests/cxx_class_signal.cpp
+++ test/cpp_test_suite/new_tests/cxx_class_signal.cpp
@@ -251,7 +251,7 @@ public:
 		pid = atoi((*result).svalue[0].in());
 		if(pid > 0)
 		#ifdef _WIN32
-		 HANDLE hProcess = OpenProcess(0x0001, FALSE, pid);
+		 void * hProcess = OpenProcess(0x0001, FALSE, pid);
 		 unsigned __int8 uExitCode = 0;
 		 TerminateProcess(hProcess, uExitCode);
 		 CloseHandle(hProcess);
