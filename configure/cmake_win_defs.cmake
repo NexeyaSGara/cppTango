a95df3ce86edbe72f776183d9d307238094f7859
NexeyaSGara <sebastien.gara@nexeya.com>
2017-11-16 16:56:24 +0100

    Update cmake_win_defs.cmake

--- configure/cmake_win_defs.cmake
+++ configure/cmake_win_defs.cmake
@@ -1,10 +1,5 @@
 #definitions and preprocessor
 add_definitions(-D_WIN32)
-add_definitions(-D__OSVERSION__=4)
-add_definitions(-D__POSIX_NT__)
-add_definitions(-D__x86__)
-add_definitions(-D__POSIX_NT__)
-add_definitions(-D__OSVERSION__=4)
 add_definitions(-DOMNI_UNLOADABLE_STUBS)
 add_definitions(-D_CRT_SECURE_NO_WARNINGS)
 if(CMAKE_CL_64)
